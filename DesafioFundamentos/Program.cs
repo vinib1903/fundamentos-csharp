d612eae8ccc03d0b33f3b007132b783b762856d7
Vinícius Bauermann Almeida <vinib1903@unesc.net>
2024-04-07 19:03:40 -0300

    desafio fundamentos csharp

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -12,14 +12,11 @@ precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
-
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
 string opcao = string.Empty;
 bool exibirMenu = true;
 
-// Realiza o loop do menu
 while (exibirMenu)
 {
     Console.Clear();
